530db35ab4e54e6dc9040fafc980555c6c822199
Aziz Kacem <azizkacem.mk@gmail.com>
2026-02-06 12:07:16 +0100

    stage 8 Interceptors&Filters

--- src/planets/planets.controller.ts
+++ src/planets/planets.controller.ts
@@ -8,6 +8,7 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { PlanetsService } from './planets.service';
+import { get } from 'http';
 
 @Controller('planets')
 export class PlanetsController {
@@ -21,6 +22,10 @@ export class PlanetsController {
   @Get()
   findAll() {
     return this.planetsService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.planetsService.findOne(id);
   }
 
   @Patch(':id/destroy')
